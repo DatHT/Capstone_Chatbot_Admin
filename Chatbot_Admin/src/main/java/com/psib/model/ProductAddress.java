6d9923a89cbb61a8e0494671334d5ddd79737e73
datht <datht0601@gmail.com>
2016-06-02 09:14:42 +0700

    update entity product_address

--- Chatbot_Admin/src/main/java/com/psib/model/ProductAddress.java
+++ Chatbot_Admin/src/main/java/com/psib/model/ProductAddress.java
@@ -13,18 +13,20 @@ import javax.persistence.Table;
 @Table(name = "Product_Address")
 public class ProductAddress implements Serializable {
 
-
 	@Id
-	@Column(name = "foodId", nullable = false)
-	private long foodId;
-	
+	@Column(name = "productId", nullable = false)
+	private long productId;
+
 	@Id
 	@Column(name = "addressId", nullable = false)
 	private long addressId;
-	
-	@Column(name = "name", nullable = false, length = 4000)
-	private String name;
-	
+
+	@Column(name = "productName", nullable = false, length = 4000)
+	private String productName;
+
+	@Column(name = "addressName", nullable = false, length = 4000)
+	private String addressName;
+
 	@Column(name = "urlrelate", nullable = false, length = 500)
 	private String urlRelate;
 
@@ -48,19 +50,19 @@ public class ProductAddress implements Serializable {
 
 	@Column(name = "districtName", nullable = false, length = 128)
 	private String districtName;
-	
-	
+
 	public ProductAddress() {
 		// TODO Auto-generated constructor stub
 	}
 
-
-	public ProductAddress(long foodId, long addressId, String name, String urlRelate, String thumbPath, String rate,
-			int numOfSearch, double longitude, double latitude, String restaurantName, String districtName) {
+	public ProductAddress(long productId, long addressId, String productName, String addressName, String urlRelate,
+			String thumbPath, String rate, int numOfSearch, double longitude, double latitude, String restaurantName,
+			String districtName) {
 		super();
-		this.foodId = foodId;
+		this.productId = productId;
 		this.addressId = addressId;
-		this.name = name;
+		this.productName = productName;
+		this.addressName = addressName;
 		this.urlRelate = urlRelate;
 		this.thumbPath = thumbPath;
 		this.rate = rate;
@@ -71,115 +73,100 @@ public class ProductAddress implements Serializable {
 		this.districtName = districtName;
 	}
 
-
-	public long getFoodId() {
-		return foodId;
+	public long getProductId() {
+		return productId;
 	}
 
-
-	public void setFoodId(long foodId) {
-		this.foodId = foodId;
+	public void setProductId(long productId) {
+		this.productId = productId;
 	}
-
 
 	public long getAddressId() {
 		return addressId;
 	}
 
-
 	public void setAddressId(long addressId) {
 		this.addressId = addressId;
 	}
 
-
-	public String getName() {
-		return name;
+	public String getAddressName() {
+		return addressName;
 	}
 
-
-	public void setName(String name) {
-		this.name = name;
+	public void setAddressName(String addressName) {
+		this.addressName = addressName;
 	}
 
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
 
 	public String getUrlRelate() {
 		return urlRelate;
 	}
 
-
 	public void setUrlRelate(String urlRelate) {
 		this.urlRelate = urlRelate;
 	}
-
 
 	public String getThumbPath() {
 		return thumbPath;
 	}
 
-
 	public void setThumbPath(String thumbPath) {
 		this.thumbPath = thumbPath;
 	}
-
 
 	public String getRate() {
 		return rate;
 	}
 
-
 	public void setRate(String rate) {
 		this.rate = rate;
 	}
-
 
 	public int getNumOfSearch() {
 		return numOfSearch;
 	}
 
-
 	public void setNumOfSearch(int numOfSearch) {
 		this.numOfSearch = numOfSearch;
 	}
-
 
 	public double getLongitude() {
 		return longitude;
 	}
 
-
 	public void setLongitude(double longitude) {
 		this.longitude = longitude;
 	}
-
 
 	public double getLatitude() {
 		return latitude;
 	}
 
-
 	public void setLatitude(double latitude) {
 		this.latitude = latitude;
 	}
-
 
 	public String getRestaurantName() {
 		return restaurantName;
 	}
 
-
 	public void setRestaurantName(String restaurantName) {
 		this.restaurantName = restaurantName;
 	}
-
 
 	public String getDistrictName() {
 		return districtName;
 	}
 
-
 	public void setDistrictName(String districtName) {
 		this.districtName = districtName;
 	}
 
-	
 }
