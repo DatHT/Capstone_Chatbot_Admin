0fb315901fa2746f405afec124aca251bae292c8
HuyTCM <huytcm.1708@gmail.com>
2016-06-06 23:27:26 +0700

    imp. create method get all file in directory and subdirectory

--- Chatbot_Admin/src/main/java/com/psib/util/FileUtils.java
+++ Chatbot_Admin/src/main/java/com/psib/util/FileUtils.java
@@ -3,13 +3,26 @@ package com.psib.util;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileUtils {
 	public static BufferedReader readFile(String filePath) throws FileNotFoundException {
 		FileReader fileReader = new FileReader(filePath);
 		BufferedReader bufferedReader = new BufferedReader(fileReader);
-		
+
 		return bufferedReader;
+	}
+
+	public static List<String> getAllFiles(String directoryPath) throws IOException {
+		List<String> listFiles = new ArrayList<String>();
+		Files.walk(Paths.get(directoryPath)).filter(Files::isRegularFile).forEach(filePath -> {
+			listFiles.add(filePath.toString());
+		});
+
+		return listFiles;
 	}
 }
