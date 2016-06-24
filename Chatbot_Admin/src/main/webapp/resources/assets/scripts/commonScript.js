5231bd5f57e0eb0c8543e4eff158637ab15b4792
datht <datht0601@gmail.com>
2016-06-24 22:23:23 +0700

    .mod example change & modify function delete example

--- Chatbot_Admin/src/main/webapp/resources/assets/scripts/commonScript.js
+++ Chatbot_Admin/src/main/webapp/resources/assets/scripts/commonScript.js
@@ -79,3 +79,15 @@ function notify(message, type){
     });
 };
 
+
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
