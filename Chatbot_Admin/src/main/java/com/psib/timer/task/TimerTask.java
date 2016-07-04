b31ca0788a6e9a6a442662dff29cec999acab05a
datht <datht0601@gmail.com>
2016-07-04 23:32:47 +0700

    .add comment code

--- Chatbot_Admin/src/main/java/com/psib/timer/task/TimerTask.java
+++ Chatbot_Admin/src/main/java/com/psib/timer/task/TimerTask.java
@@ -28,29 +28,28 @@ import com.psib.service.impl.PhraseManager;
 @Service
 public class TimerTask {
 
-    private static final Logger LOG = Logger.getLogger(TimerTask.class);
-
+	private static final Logger LOG = Logger.getLogger(TimerTask.class);
 
 	@Autowired
 	private ILexicalCategoryManager lexicalManager;
 	@Autowired
 	private IPhraseManager phraseManager;
-    
-    public void synchronizeFromAPItoDB() {
-    	LOG.info("[doTimer] Start");
-    	try {
-    		List<LexicalCategoryDto> lexicals = lexicalManager.getApiLexicals();
-    		for (LexicalCategoryDto dto : lexicals) {
-    			long id = 0;
-    			if ((id = lexicalManager.checkExistLexical(dto.getName())) == -1) {
-    				// sync from lexical to db
-    				LexicalCategory lexical = new LexicalCategory();
-    				lexical.setName(dto.getName());
-    				lexical.setLastModify(new Date());
-    				id = lexicalManager.insertLexicalToDatabase(lexical);
-    				LOG.info("[Sync lexical phrase done]");
-    			}
-    			LexicalDto entry = lexicalManager.getApiLexicalById(String.valueOf(dto.getId()));
+
+	public void synchronizeFromAPItoDB() {
+		LOG.info("[doTimer] Start");
+		try {
+			List<LexicalCategoryDto> lexicals = lexicalManager.getApiLexicals();
+			for (LexicalCategoryDto dto : lexicals) {
+				long id = 0;
+				if ((id = lexicalManager.checkExistLexical(dto.getName())) == -1) {
+					// sync from lexical to db
+					LexicalCategory lexical = new LexicalCategory();
+					lexical.setName(dto.getName());
+					lexical.setLastModify(new Date());
+					id = lexicalManager.insertLexicalToDatabase(lexical);
+					LOG.info("[Sync lexical phrase done]");
+				}
+				LexicalDto entry = lexicalManager.getApiLexicalById(String.valueOf(dto.getId()));
 				for (Entry item : entry.getEntries()) {
 					Phrase phrase = phraseManager.checkExist(item.getValue());
 					if (phrase == null) {
@@ -62,63 +61,67 @@ public class TimerTask {
 						LOG.info("[Sync insert phrase done]");
 					}
 				}
-    		}
-            LOG.info("[doTimer] End");
-    	}catch (IOException e) {
-    		LOG.error("[Sync error] " + e.getMessage());
-    	} catch (RestfulException e) {
-    		LOG.error("[Sync error] " + e.getMessage());
+			}
+			LOG.info("[doTimer] End");
+		} catch (IOException e) {
+			LOG.error("[Sync error] " + e.getMessage());
+		} catch (RestfulException e) {
+			LOG.error("[Sync error] " + e.getMessage());
 		}
 
-    }
-    
-    public void synchronizeFromDBToAPI() {
-//		try {
-//			List<Phrase> phrases = phraseManager.getAll();
-//			for (int i = 0; i < phrases.size(); i++) {
-//				Phrase p = phrases.get(i);
-//				Entry entry = new Entry();
-//				entry.setValue(p.getName());
-//				List<String> synonym = new ArrayList<>();
-//				synonym.add(p.getName());
-//				entry.setSynonyms(synonym);
-//				StatusCode code = lexicalManager.addPhrase(entry, "bf8bb68e-dde4-4a7a-ac7c-9d4101e9aaf7");
-//				switch (code) {
-//				case SUCCESS:
-//					LOG.info("[Sync to api] success ");
-//					break;
-//				case ERROR:
-//					LOG.info("[Sync to api] error ");
-//					break;
-//				case CONFLICT:
-//					LOG.info("[Sync to api] conflict ");
-//					break;
-//				}
-//				LOG.info("[Sync to api] done " + i);
-//			}
-//		}catch (IOException e) {
-//    		LOG.error("[Sync error] " + e.getMessage());
-//    	} catch (RestfulException e) {
-//    		LOG.error("[Sync error] " + e.getMessage());
-//		}
 	}
-    
-  //delete phrase in api
-//	LexicalDto food = lexicalManager.getApiLexicalById("bf8bb68e-dde4-4a7a-ac7c-9d4101e9aaf7");
-//	List<String> values = new ArrayList<>();
-//	for(Entry e : food.getEntries()) {
-//		values.add(e.getValue());
-//	}
-//	StatusCode code1 = lexicalManager.deletePhrase(values, food.getName());
-//	switch (code1) {
-//	case SUCCESS:
-//		logger.info("[Delete to api] success ");
-//		break;
-//	case ERROR:
-//		logger.info("[Delete to api] error ");
-//		break;
-//	case CONFLICT:
-//		logger.info("[Delete to api] conflit ");;
-//		break;
-//	}
+
+	// TODO: use for future plan
+	public void synchronizeFromDBToAPI() {
+		// try {
+		// List<Phrase> phrases = phraseManager.getAll();
+		// for (int i = 0; i < phrases.size(); i++) {
+		// Phrase p = phrases.get(i);
+		// Entry entry = new Entry();
+		// entry.setValue(p.getName());
+		// List<String> synonym = new ArrayList<>();
+		// synonym.add(p.getName());
+		// entry.setSynonyms(synonym);
+		// StatusCode code = lexicalManager.addPhrase(entry,
+		// "bf8bb68e-dde4-4a7a-ac7c-9d4101e9aaf7");
+		// switch (code) {
+		// case SUCCESS:
+		// LOG.info("[Sync to api] success ");
+		// break;
+		// case ERROR:
+		// LOG.info("[Sync to api] error ");
+		// break;
+		// case CONFLICT:
+		// LOG.info("[Sync to api] conflict ");
+		// break;
+		// }
+		// LOG.info("[Sync to api] done " + i);
+		// }
+		// }catch (IOException e) {
+		// LOG.error("[Sync error] " + e.getMessage());
+		// } catch (RestfulException e) {
+		// LOG.error("[Sync error] " + e.getMessage());
+		// }
+	}
+
+	// TODO: use for use again in delete function
+	// delete phrase in api
+	// LexicalDto food =
+	// lexicalManager.getApiLexicalById("bf8bb68e-dde4-4a7a-ac7c-9d4101e9aaf7");
+	// List<String> values = new ArrayList<>();
+	// for(Entry e : food.getEntries()) {
+	// values.add(e.getValue());
+	// }
+	// StatusCode code1 = lexicalManager.deletePhrase(values, food.getName());
+	// switch (code1) {
+	// case SUCCESS:
+	// logger.info("[Delete to api] success ");
+	// break;
+	// case ERROR:
+	// logger.info("[Delete to api] error ");
+	// break;
+	// case CONFLICT:
+	// logger.info("[Delete to api] conflit ");;
+	// break;
+	// }
 }
