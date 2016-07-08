eb727ac917b192d6da7c4b2f32d8e2784c6129be
HuyTCM <huytcm.1708@gmail.com>
2016-07-09 00:20:12 +0700

    fix. or condition and check some more condition

--- Chatbot_Admin/src/main/java/com/psib/util/CronExpressionUtils.java
+++ Chatbot_Admin/src/main/java/com/psib/util/CronExpressionUtils.java
@@ -10,22 +10,24 @@ public class CronExpressionUtils {
 	}
 
 	public static String weekly(DayOfWeek weekday) throws CronExpressionException {
-		return CronExpressionUtils.convertCronExpression(0, 0, 0, "?", "*", weekday);
+		return CronExpressionUtils.convertCronExpression(0, 0, 0, weekday == DayOfWeek.Unknown ? "*" : "?", "*",
+				weekday);
 	}
-	
+
 	public static String weekly(DayOfWeek weekday, int second, int minute, int hour) throws CronExpressionException {
-		return CronExpressionUtils.convertCronExpression(second, minute, hour, "?", "*", weekday);
+		return CronExpressionUtils.convertCronExpression(second, minute, hour, weekday == DayOfWeek.Unknown ? "*" : "?",
+				"*", weekday);
 	}
 
 	public static String monthly(int day) throws CronExpressionException {
-		if (0 > day && day > 31) {
+		if (0 > day || day > 31) {
 			throw new CronExpressionException("Invalid day!");
 		}
 		return CronExpressionUtils.convertCronExpression(0, 0, 0, String.valueOf(day), "1/1", DayOfWeek.Unknown);
 	}
-	
+
 	public static String monthly(int day, int second, int minute, int hour) throws CronExpressionException {
-		if (0 > day && day > 31) {
+		if (0 > day || day > 31) {
 			throw new CronExpressionException("Invalid day!");
 		}
 		return CronExpressionUtils.convertCronExpression(second, minute, hour, String.valueOf(day), "1/1",
@@ -34,15 +36,15 @@ public class CronExpressionUtils {
 
 	private static String convertCronExpression(int second, int minute, int hour, String day, String month,
 			DayOfWeek weekday) throws CronExpressionException {
-		if (0 > second && second > 59) {
+		if (0 > second || second > 59) {
 			throw new CronExpressionException("Invalid second!");
 		}
 
-		if (0 > minute && minute > 59) {
+		if (0 > minute || minute > 59) {
 			throw new CronExpressionException("Invalid minute!");
 		}
 
-		if (0 > hour && hour > 23) {
+		if (0 > hour || hour > 23) {
 			throw new CronExpressionException("Invalid hour!");
 		}
 
